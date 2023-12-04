fcf9c0a63b4fb5bb8a3d9263819098b4a0837186
mohas191 <60118548+mohas191@users.noreply.github.com>
2023-12-03 22:07:32 -0500

    mini oush

--- wwwroot/PhotosManager/js/siteUI.js
+++ wwwroot/PhotosManager/js/siteUI.js
@@ -14,9 +14,18 @@ function saveContentScrollPosition() {
 function restoreContentScrollPosition() {
     $("#content")[0].scrollTop = contentScrollPosition;
 }
-function updateHeader() {
-    //todo
+
+setTimeout(function(){ // reload chaque seconde
+    window.location.reload();
+ }, 2000);
+
+import {get as getHeader} from "./views/header.js";
+
+function UpdateHeader() {
+    $("#header").empty();
+    $("#header").append(getHeader());
 }
+
 function renderAbout() {
     timeout();
     saveContentScrollPosition();
@@ -40,4 +49,31 @@ function renderAbout() {
                 </p>
             </div>
         `))
+}
+
+renderAbout();
+
+function renderCreateProfil() {
+    noTimeout(); // ne pas limiter le temps d’inactivité
+    eraseContent(); // effacer le conteneur #content
+    UpdateHeader("Inscription", "createProfil"); // mettre à jour l’entête et menu
+    $("#newPhotoCmd").hide(); // camouffler l’icone de commande d’ajout de photo
+    $("#content").append(`
+    …html du formulaire…
+    `);
+    $('#loginCmd').on('click', renderLoginForm); // call back sur clic
+    initFormValidation();
+    initImageUploaders();
+    $('#abortCmd').on('click', renderLoginForm); // call back sur clic
+    // ajouter le mécanisme de vérification de doublon de courriel
+    addConflictValidation(API.checkConflictURL(), 'Email', 'saveUser');
+    // call back la soumission du formulaire
+    $('#createProfilForm').on("submit", function (event) {
+        let profil = getFormData($('#createProfilForm'));
+        delete profil.matchedPassword;
+        delete profil.matchedEmail;
+        event.preventDefault();// empêcher le fureteur de soumettre une requête de soumission
+        showWaitingGif(); // afficher GIF d’attente
+        createProfil(profil); // commander la création au service API
+    });
 }
